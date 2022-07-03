e26c4cd42a97e5e6575a7d5f62e2496eeedfab77
wonjinYi <studioplug17@gmail.com>
2022-07-04 04:05:57 +0900

    [Bronze V] Title: Julka, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/8437. Julka/Julka.py
@@ -0,0 +1,35 @@
+a = int(input())
+b = int(input())
+'''
+13
+3
+
+1 12 11
+2 11 9
+3 10 7
+4 9 5
+5 8 3
+4 9 5 8
+
+6
+6
+
+10
+4
+
+1 9  8
+2 8  6
+3 7  4
+4 6  2
+5 5  0
+
+5 2
+5 2'''
+half = a//2
+small = b//2
+big = b-small
+
+print(half+big)
+print(half-small)
+  
+  

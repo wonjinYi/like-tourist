aee7750c9a72fe2ea725db62ab09245e9544eb16
wonjinYi <studioplug17@gmail.com>
2022-07-09 02:11:39 +0900

    [Bronze I] Title: 무알콜 칵테일, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2896. 무알콜 칵테일/무알콜 칵테일.cc
+++ 백준/Bronze/2896. 무알콜 칵테일/무알콜 칵테일.cc
@@ -12,34 +12,10 @@ int main() {
   cin >> a >> b >> c;
   cin >> x >> y >> z;
 
-  int v, w, u;
-
-  // int round = 1;
-  // int n = 0;
-  // while(true){
-  //   v = x*round; w = y*round; u = z*round;
-  //   if(x-v <= 0){
-  //     n = 0;
-  //     break;
-  //   } else if (y-w <= 0) {
-  //     n = 1;
-  //     break;
-  //   } else if (z-u <= 0) {
-  //     n = 2;
-  //     break;
-  //   }
-  // }
-
-  // double gorani = 0;
-  // if(n==0) gorani = (double)a / x;
-  // else if(n==1) gorani = (double)b / y;
-  // else if(n==2) gorani = (double)c / z;
-
   double arr[3] = {(double)a/x, (double)b/y, (double)c/z};
   sort(arr, arr+3);
   double gorani = arr[0];
 
-  
   cout.precision(6);
   cout.setf(ios::fixed);
   double res[3] = { a - x*gorani, b - y*gorani, c - z*gorani };
@@ -55,21 +31,4 @@ int main() {
     if( i<2 )
       cout << " ";
   }
-  
-  //cout.setf(ios::fixed);
-  //cout.precision(6);
-
-  double ar = a - x*gorani;
-  double br = b - y*gorani;
-  double cr = c - z*gorani;
-
-  //cout << fixed << setprecision(6) << ar << " " << br << " " << cr;
-  //cout << fixed << setprecision(6) << a - x*gorani << " " << b - y*gorani << " " << c - z*gorani;
-  
-  // cout  << (int(ar) <= ar ? int(ar) : ar) << " " 
-  //       << (int(br) <= br ? int(br) : br) << " " 
-  //       << (int(cr) <= cr ? int(cr) : cr);
-
-  //cout << ((15.0000 == 15) ? "yes" : "no") << endl;
-  
 }

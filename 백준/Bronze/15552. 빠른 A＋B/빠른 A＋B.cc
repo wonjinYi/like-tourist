319f6eb03e07da23d533d8f8b392efd1fefecafe
wonjinYi <studioplug17@gmail.com>
2022-07-10 07:02:35 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 224 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cin.tie(NULL);
+  ios::sync_with_stdio(false);
+
+  int n, a, b;
+  cin >> n;
+  for(int i=0; i<n; i++){
+    cin >> a >> b;
+    cout << a+b << "\n";
+  }
+  
+}
